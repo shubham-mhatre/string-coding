1a8ffe0f608ee871c264b2663d8ad0654d91d745
shubham <mhatreshubham03@gmail.com>
2025-01-26 20:35:33 +0530

    project setup initial commit

---
+++ src/main/java/com/str/cd/StringCodingApplication.java
@@ -0,0 +1,13 @@
+package com.str.cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/str/cd/StringCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.str.cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
